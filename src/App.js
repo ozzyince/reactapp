35ba8686025c29022f372fb16b5e25a59d2e91bc
Özgür İnce <ozgur@ince.ist>
2018-11-26 18:03:52 +0300

    install @babel/plugin-proposal-class-properties, modify login form introduce constructor, state and event handlers, property initializer syntax

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React, { Component} from 'react';
 
 export default class App extends Component{
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    alert(JSON.stringify(this.state));
+  }
+
   render() {
     return(
-      <form>
-        <input type="text" name="username" />
-        <input type="password" name="password" />
+      <form onSubmit={this.handleSubmit}>
+        <input id="username" type="text" name="username" value={this.state.username} onChange={this.handleChange} />
+        <input id="password" type="password" name="password" value={this.state.password} onChange={this.handleChange} />
         <button type="submit">Login</button>
       </form>
     );
